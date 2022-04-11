a14332234fab58df4a5fb6cc3915582ff1bc58dc
ewanfulmer85 <ewan.fulmer@gmail.com>
2022-04-11 14:25:17 -0500

    added warning pop up window

--- gui.py
+++ gui.py
@@ -6,6 +6,7 @@ import subprocess
 import pyautogui
 import time
 import threading
+from ctypes import *
 
 
 # Functions
@@ -24,65 +25,88 @@ def run_customer_installer(windows_username_, windows_password_, directory):
     p = subprocess.Popen(['runas', '/profile', '/user:' + windows_username_,
                           directory],
                          creationflags=subprocess.CREATE_NEW_CONSOLE)
-    time.sleep(2)
+
+    ok = windll.user32.BlockInput(True)
+    time.sleep(5)
     pyautogui.typewrite(windows_password_)
     pyautogui.press('enter')
+    time.sleep(2)
     pyautogui.typewrite("I")
     pyautogui.press('enter')
-    time.sleep(10)
+    time.sleep(5)
     pyautogui.press('enter')
+    ok = windll.user32.BlockInput(False)
     window.write_event_value("-CI THREAD DONE-", "DONE")
 
 
 def run_installer(host_name_, db_username_, db_password_, logical_db_name_, windows_username_, windows_password_,
-                  directory):
+                  directory, event_obj):
     directory += "/bin/installer.bat"
     p = subprocess.Popen(['runas', '/profile', '/user:' + windows_username_,
                           directory],
                          creationflags=subprocess.CREATE_NEW_CONSOLE)
-    time.sleep(3)
+    ok = windll.user32.BlockInput(True)
+    time.sleep(5)
     pyautogui.typewrite(windows_password_)
     pyautogui.press('enter')
-    time.sleep(10)
+    time.sleep(5)
     pyautogui.typewrite("1")
     pyautogui.press('enter')
-    time.sleep(2)
+    time.sleep(1)
     pyautogui.typewrite("1")
     pyautogui.press('enter')
-    time.sleep(2)
+    time.sleep(1)
     pyautogui.typewrite(host_name_)
     pyautogui.press('enter')
-    time.sleep(2)
+    time.sleep(1)
     pyautogui.press('enter')
-    time.sleep(2)
+    time.sleep(1)
     pyautogui.press('enter')
-    time.sleep(2)
+    time.sleep(1)
     pyautogui.press('enter')
-    time.sleep(2)
+    time.sleep(1)
     pyautogui.typewrite(db_username_)
     pyautogui.press('enter')
-    time.sleep(2)
+    time.sleep(1)
     pyautogui.typewrite(db_password_)
     pyautogui.press('enter')
-    time.sleep(2)
+    time.sleep(1)
     pyautogui.typewrite(logical_db_name_)
     pyautogui.press('enter')
-    time.sleep(2)
+    time.sleep(1)
     pyautogui.press('enter')
-    time.sleep(2)
+    time.sleep(1)
     pyautogui.press('enter')
-    time.sleep(2)
+    time.sleep(1)
     pyautogui.press('enter')
-    time.sleep(2)
+    time.sleep(1)
     pyautogui.press('enter')
-    time.sleep(2)
+    time.sleep(1)
     pyautogui.press('enter')
-    time.sleep(2)
+    time.sleep(1)
     pyautogui.press('enter')
-    time.sleep(2)
+    time.sleep(1)
     pyautogui.press('enter')
-    time.sleep(2)
+    time.sleep(1)
     pyautogui.press('enter')
+    ok = windll.user32.BlockInput(False)
+    window.write_event_value("-WARNING-", "warning")
+    flag = event_obj.wait()
+    if flag:
+        pyautogui.typewrite('y')
+        pyautogui.press('enter')
+        time.sleep(1)
+        pyautogui.typewrite('delete')
+        pyautogui.press('enter')
+        time.sleep(1)
+        pyautogui.press('enter')
+        time.sleep(1)
+        pyautogui.press('enter')
+        time.sleep(1)
+        pyautogui.press('enter')
+        time.sleep(3)
+        pyautogui.typewrite('I')
+        pyautogui.press('enter')
 
 
 
@@ -122,12 +146,17 @@ run_installer_page = [[sg.Text('Run Installer', font=('Arial', 18), size=(40, 2)
                       [sg.Text('Press continue to run the installer script', size=(40, 3))],
                       [sg.Button('Continue', key="ri_continue")]]
 
+warning_page = [[sg.Text('Warning!', font=('Arial', 18), size=(40, 2))],
+                [sg.Text('Are you sure you want to drop and recreate database?', size=(40, 3))],
+                [sg.Button('Yes', key="wap_yes")],
+                [sg.Button('No', key="wap_no")]]
 
 layout = [[sg.Column(welcome_page, key='-COL1-'), sg.Column(folder_select_page, visible=False, key='-COL2-'),
            sg.Column(get_credentials_page, visible=False, key='-COL3-'),
            sg.Column(run_customer_installer_page, visible=False, key='-COL4-'),
            sg.Column(get_info_installer, visible=False, key='-COL5-'),
-           sg.Column(run_installer_page, visible=False, key='-COL6-')]]
+           sg.Column(run_installer_page, visible=False, key='-COL6-'),
+           sg.Column(warning_page, visible=False, key='-COL7-')]]
 
 # Start GUI
 window = sg.Window('Oasis Build Installer', layout, resizable=True)
@@ -142,6 +171,7 @@ host_name = ""
 db_username = ""
 db_password = ""
 logical_db_name = ""
+event_obj = threading.Event()
 while True:
     event, values = window.read()
     if event in (sg.WIN_CLOSED, "Exit"):
@@ -153,14 +183,14 @@ while True:
         if folder_location == "":
             sg.Print('No folder selected')
         else:
+            extract_install_package(folder_location, path_to_install_zip_file)
+            move_customer_package(folder_location, path_to_customer_zip_file)
             window[f'-COL2-'].update(visible=False)
             window[f'-COL3-'].update(visible=True)
     if event == '-FOLDER-':
         folder_location = values['-FOLDER-']
         path_to_install_zip_file = folder_location + "/argo-fraud-4.2.7.0-install-package (4).zip"
         path_to_customer_zip_file = folder_location + "/argo-customer-fraud-argo-4.2.7.0-package (5).zip"
-        extract_install_package(folder_location, path_to_install_zip_file)
-        move_customer_package(folder_location, path_to_customer_zip_file)
     if event == 'gc_continue':
         windows_username = values[0]
         windows_password = values[1]
@@ -182,8 +212,15 @@ while True:
         window[f'-COL6-'].update(visible=True)
     if event == 'ri_continue':
         threading.Thread(target=run_installer, args=(
-            host_name, db_username, db_password, logical_db_name, windows_username, windows_password, folder_location),
+            host_name, db_username, db_password, logical_db_name, windows_username, windows_password, folder_location,
+            event_obj),
                          daemon=True).start()
+    if event == '-WARNING-':
+        if sg.Window("Warning!", [[sg.Text("Drop and recreate the database?\nThis action is non-reversible!")],
+                                  [sg.Yes(), sg.No()]]).read(close=True)[0] == "Yes":
+                event_obj.set()
+        else:
+            break
 
 
 
