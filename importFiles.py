18c01f868d4e8ca76ad9d423c15510735090c236
crissr11 <43736409+crissr11@users.noreply.github.com>
2022-04-18 20:46:26 -0500

    transition to GUI 2

--- importFiles.py
+++ importFiles.py
@@ -7,73 +7,43 @@ import time
 import PySimpleGUI as sg
 import win32serviceutil
 
+
 # Functions
-def = extract_file_package(directory, install_zip):
+def extract_file_package(directory, install_zip):
     with zipfile.ZipFile(install_zip, 'r') as zip_ref:
         zip_ref.extractall(directory)
 
 
+def move_files(directory, files_zip):
+    shutil.move(files_zip, (directory + ""))
+
+
 # Layout Pages
 sg.theme("SystemDefaultForReal")
-welcome_page = [[sg.Text('Oasis Build Installation', font=('Arial', 18), size=(40, 2))],
-                [sg.Text('Press continue to start the Oasis build installation process', size=(40, 3))],
+welcome_page = [[sg.Text('Oasis Import Files', font=('Arial', 18), size=(40, 2))],
+                [sg.Text('Press continue to start the Oasis file import process', size=(40, 3))],
                 [sg.Button('Continue', key="wp_continue")]]
 
 folder_select_page = [[sg.Text('Select Folder', font=('Arial', 18), size=(40, 2))],
-                      [sg.Text('Select the folder containing zip files', size=(40, 3))],
+                      [sg.Text('Select the folder containing zip file', size=(40, 3))],
                       [sg.Text('Folder'), sg.In(size=(25, 1), enable_events=True, key='-FOLDER-'), sg.FolderBrowse()],
                       [sg.Text('', size=(18, 1))],
                       [sg.Button('Continue', key="fs_continue")]]
-
-get_credentials_page = [[sg.Text('Windows Credentials', font=('Arial', 18), size=(40, 2))],
-                        [sg.Text('Enter credentials to be able to run scripts as administrator', size=(40, 4))],
-                        [sg.Text('Windows username ', size=(18, 1)), sg.InputText()],
-                        [sg.Text('Windows password ', size=(18, 1)), sg.InputText(password_char="*")],
-                        [sg.Text('', size=(18, 1))],
-                        [sg.Button('Continue', key="gc_continue")]]
 
 run_customer_installer_page = [[sg.Text('Run Customer Installer', font=('Arial', 18), size=(40, 2))],
                                [sg.Text('Press continue to run the customer installer script', size=(40, 3))],
                                [sg.Button('Continue', key="rci_continue")]]
 
-get_info_installer = [[sg.Text('Installer Info', font=('Arial', 18), size=(40, 2))],
-                      [sg.Text('Enter information to be able to run the installer script', size=(40, 4))],
-                      [sg.Text('Hostname ', size=(18, 1)), sg.InputText()],
-                      [sg.Text('Database Username ', size=(18, 1)), sg.InputText()],
-                      [sg.Text('Database Password ', size=(18, 1)), sg.InputText(password_char="*")],
-                      [sg.Text('Logical DB Name ', size=(18, 1)), sg.InputText()],
-                      [sg.Text('', size=(18, 1))],
-                      [sg.Button('Continue', key="gii_continue")]]
-
-run_installer_page = [[sg.Text('Run Installer', font=('Arial', 18), size=(40, 2))],
-                      [sg.Text('Press continue to run the installer script', size=(40, 3))],
-                      [sg.Button('Continue', key="ri_continue")]]
-
-warning_page = [[sg.Text('Warning!', font=('Arial', 18), size=(40, 2))],
-                [sg.Text('Are you sure you want to drop and recreate database?', size=(40, 3))],
-                [sg.Button('Yes', key="wap_yes")],
-                [sg.Button('No', key="wap_no")]]
-
 layout = [[sg.Column(welcome_page, key='-COL1-'), sg.Column(folder_select_page, visible=False, key='-COL2-'),
-           sg.Column(get_credentials_page, visible=False, key='-COL3-'),
-           sg.Column(run_customer_installer_page, visible=False, key='-COL4-'),
-           sg.Column(get_info_installer, visible=False, key='-COL5-'),
-           sg.Column(run_installer_page, visible=False, key='-COL6-'),
-           sg.Column(warning_page, visible=False, key='-COL7-')]]
+           sg.Column(run_customer_installer_page, visible=False, key='-COL3-')]]
 
 # Start GUI
-window = sg.Window('Oasis Build Installer', layout, resizable=True,)
+window = sg.Window('Oasis Import Files', layout, resizable=True, )
 # Behavior of GUI
 folder_location = ""
 path_to_install_zip_file = ""
 path_to_customer_zip_file = ""
 customer_folder_location = ""
-windows_username = ""
-windows_password = ""
-host_name = ""
-db_username = ""
-db_password = ""
-logical_db_name = ""
 event_obj = threading.Event()
 while True:
     event, values = window.read()
@@ -86,8 +56,8 @@ while True:
         if folder_location == "":
             sg.Print('No folder selected')
         else:
-            extract_install_package(folder_location, path_to_install_zip_file)
-            move_customer_package(folder_location, path_to_customer_zip_file)
+            extract_file_package(folder_location, path_to_install_zip_file)
+            move_files(folder_location, path_to_customer_zip_file)
             window[f'-COL2-'].update(visible=False)
             window[f'-COL3-'].update(visible=True)
     if event == '-FOLDER-':
@@ -118,12 +88,6 @@ while True:
             host_name, db_username, db_password, logical_db_name, windows_username, windows_password, folder_location,
             event_obj),
                          daemon=True).start()
-    if event == '-WARNING-':
-        if sg.Window("Warning!", [[sg.Text("Drop and recreate the database?\nThis action is non-reversible!")],
-                                  [sg.Yes(), sg.No()]]).read(close=True)[0] == "Yes":
-            event_obj.set()
-        else:
-            break
     if event == '-I THREAD DONE-':
         break
 
@@ -148,6 +112,10 @@ shutil.move(path_to_xml_file, directory_to_move_to)
 serviceName = "ArgoFraudComplianceService"
 # win32serviceutil.StartService(serviceName)
 # win32serviceutil.RestartService("ArgoFraudComplianceService")
+
+# Run CIF import
+webbrowser.open_new('http://localhost:8084/fcs-webservice/jolokia/exec/com.argodata.fraud:name=cifImportJmxService,'
+                    'type=CifImportJmxService/runCifImportNow/1')
 
 # Get file paths to each import location and move files
 path_to_cif_import_files = "C:\\Users\\Nebula\\Desktop\\On_US_DATA\\CIF\\Richard CIF file"
@@ -178,6 +146,4 @@ allfiles = os.listdir(path_to_cash_letter_import_files_2)
 for f in allfiles:
     shutil.move(path_to_cash_letter_import_files_2 + "\\" + f, cash_letter_directory_to_move_to + "\\" + f)
 
-# Run CIF import
-webbrowser.open_new('http://localhost:8084/fcs-webservice/jolokia/exec/com.argodata.fraud:name=cifImportJmxService,'
-                    'type=CifImportJmxService/runCifImportNow/1')
+
